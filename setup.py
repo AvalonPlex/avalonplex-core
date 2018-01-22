a2c148ba7eaf61d61b697d37381e42911306bb14
Joshua Avalon <joshuaavalon@gmail.com>
2018-01-22 15:16:34 +0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+
+setup(name="avalonplex-core",
+      version="1.0.0",
+      author="Avalon Plex",
+      url="https://github.com/AvalonPlex/avalonplex-core",
+      py_modules=["avalonplex_core"])
