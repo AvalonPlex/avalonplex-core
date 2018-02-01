9641cd8f3efdaa5e9cbe5660a6b52df15024f045
Joshua Avalon <joshuaavalon@gmail.com>
2018-02-01 12:24:48 +0800

    Update version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name="avalonplex-core",
-      version="1.0.2",
+      version="1.0.3",
       author="Avalon Plex",
       url="https://github.com/AvalonPlex/avalonplex-core",
       python_requires=">=3.6",
